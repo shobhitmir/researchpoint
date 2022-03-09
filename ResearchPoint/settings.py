2dccb7f46b2e3a34bedbf6ffdbf9a1b70cc46ec7
Shobhit <shobhitmir@gmail.com>
2022-03-09 15:34:47 +0530

    added User, Paper and Upvote tables

--- ResearchPoint/settings.py
+++ ResearchPoint/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+AUTH_USER_MODEL = "recommender.User"
 # Application definition
 
 INSTALLED_APPS = [
--- recommender/models.py
+++ recommender/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    interests = models.TextField(null=True,blank=True)
+
+class Paper(models.Model):
+    paper_title = models.CharField(max_length = 100,null=False,blank=False)
+    paper_abstract = models.TextField(null=True,blank=True)
+
+class Upvote(models.Model):
+    user = models.OneToOneField(User,
+    on_delete=models.CASCADE, null=True, blank=True)
+    paper = models.OneToOneField(Paper,
+    on_delete=models.CASCADE, null=True, blank=True)
+    
